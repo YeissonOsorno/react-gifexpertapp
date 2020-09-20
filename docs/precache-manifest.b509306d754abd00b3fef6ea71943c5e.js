a5df4a3a960063e8282259d0980c3f391f9cc5e5
YeissonOsorno <yeisson403@gmail.com>
2020-09-20 15:57:54 -0500

    Add Docs

---
+++ docs/precache-manifest.b509306d754abd00b3fef6ea71943c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0ad0001fb7107f5a5c12337f1057e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a5fd61a0b83fa6ef5f6",
+    "url": "/static/css/main.39425c43.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5fd61a0b83fa6ef5f6",
+    "url": "/static/js/main.ea027795.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
